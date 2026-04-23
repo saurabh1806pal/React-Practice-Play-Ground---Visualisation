0e7dd2c53c15a1bfe8b1ec999b20e39a4cf0c9bd
Saurabh Pal <94759358+saurabh1806pal@users.noreply.github.com>
2026-04-23 17:41:17 +0530

    Redux-Toolkit-Commit

---
+++ redux-toolkit-overview/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+
+const App = () => {
+	return (
+		<div className="app">
+			<h1 className="app-title">My Tasks</h1>
+			<AddTodo />
+			<TodoList />
+		</div>
+	);
+};
+
+export default App;
